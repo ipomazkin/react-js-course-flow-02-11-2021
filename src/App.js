eeaae77e0d8851584697ae01850e8bb79e5c9fb3
zenetix <mnogolikiy.ilay@gmail.com>
2021-11-02 18:07:34 +0200

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import { Lesson as Lesson1 } from "./lessons/01";
+
+function App() {
+  return (
+    <Lesson1 />
+  );
+}
+
+export default App;
---
+++ src/lessons/01/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export function Lesson() {
+  return (
+    <div className="lesson">
+
+    </div>
+  );
+}
+
+Lesson.propTypes = {};
