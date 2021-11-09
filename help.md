9d36888f6398778122cf9d2929ac3da854443ea6
zenetix <mnogolikiy.ilay@gmail.com>
2021-11-09 18:08:41 +0200

    lesson 1 code

---
+++ help.md
@@ -0,0 +1,7 @@
+```bash
+
+npx create-react-app my-app
+npm start
+yarn start
+
+```
--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import { Lesson as Lesson1 } from "./lessons/01";
 
+
 function App() {
+  let [isOpen, setIsOpen] = useState(false);
+
   return (
-    <Lesson1 />
+    <div>
+      <h1 id={[1, 2, 3].join('.')}>{(new Date()).getDate()}</h1>
+      <p>Text</p>
+      <button onClick={() => setIsOpen(!isOpen)}>click me</button>
+      {isOpen && (
+        <h3 className="">opened</h3>
+      )}
+    </div>
   );
 }
 
